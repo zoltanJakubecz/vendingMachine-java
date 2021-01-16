a5aa2ddc9b7c8975e99dccbc653da199f491049b
Zoltan Jakubecz <jakuzo@gmail.com>
2021-01-16 10:05:18 +0100

    Add Coins class

---
+++ src/main/java/com/jakuza/vendingmachine/model/Coins.java
@@ -0,0 +1,27 @@
+package com.jakuza.vendingmachine.model;
+
+public enum Coins {
+
+    PENNY("penny", 1),
+    NICKEL("nickel", 5),
+    DIME("dime", 10),
+    QUARTER("quarter", 25);
+
+    private final String name;
+    private final Integer value;
+
+    Coins(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+
+}
