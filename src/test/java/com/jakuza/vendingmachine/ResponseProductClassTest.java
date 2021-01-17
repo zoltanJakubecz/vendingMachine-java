a29c2e05f0681d50e5ce8690532d3d3bb8b53d1f
Zoltan Jakubecz <jakuzo@gmail.com>
2021-01-17 09:37:27 +0100

    add tests to ResponseProduct class test

--- src/test/java/com/jakuza/vendingmachine/ResponseProductClassTest.java
+++ src/test/java/com/jakuza/vendingmachine/ResponseProductClassTest.java
@@ -8,9 +8,20 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ResponseProductClassTest {
 
+    ResponseProduct responseProduct = new ResponseProduct(new Product("Test", 0), 0, "Test");
+
     @Test
     void sanityCheck() {
-        ResponseProduct responseProduct = new ResponseProduct(new Product("Test", 0), 0);
         assertThat(responseProduct).isNotNull();
+    }
+
+    @Test
+    void getProduct() {
+        assertThat(responseProduct.getProduct()).isNotNull();
+    }
+
+    @Test
+    void getRightMessage() {
+        assertThat(responseProduct.getMessage()).isEqualTo("Test");
     }
 }
